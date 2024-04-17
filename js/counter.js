1a3a8d06d12b0e842af798e8bdd58c17949d8f6f
Tim <tim@egirl.ceo>
2024-04-17 15:45:40 +0200

    Add: hours and minutes for counter

--- js/counter.js
+++ js/counter.js
@@ -1,8 +1,10 @@
 window.onload = function () {
     setInterval(() => {
         let time = new Date(2024, 12, 24) - new Date();
-        let diff = Math.round(time / (1000 * 60 * 60 * 24));
-        console.log(diff)
-        window.document.getElementById("time").innerText = diff + " days";
+        let diffDays = Math.round(time / (1000 * 60 * 60 * 24));
+        let diffHours = Math.round((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let diffMinutes = Math.round((time % (1000 * 60 * 60)) / (1000 * 60));
+        
+        window.document.getElementById("time").innerText = diffDays + ":" + diffHours + ":" + diffMinutes;
     }, 1000);
 };
