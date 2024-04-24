322ec7ad0f1aec037b3d5f45e4c70a25a6427fa9
Tim Arnold <26187756+degenerateddev@users.noreply.github.com>
2024-04-24 21:03:03 +0200

    Add: social links

--- js/socials_commands.js
+++ js/socials_commands.js
@@ -89,18 +89,28 @@ document.addEventListener('keypress', function(event) {
                         <td>2024-09-01 12:00:00</td>
                         <td>/linkedin</td>
                     </tr>
+                    <tr>
+                        <td>-rwxr-xr-x</td>
+                        <td>default</td>
+                        <td>default</td>
+                        <td>4096</td>
+                        <td>2024-09-01 12:00:00</td>
+                        <td>/github</td>
+                    </tr>
                 </table>
             `;
         } else if ((val === "cd ..") || (val === "cd /")) {
             window.location.href = "../index.html";
         } else if ((val === "cd /writing") || (val === "cd writing") || (val === "cd writing/")) {
-            window.location.href = "https://story.one/tim-arnold";
+            window.location.href = "https://www.story.one/en/author/tim-arnold-31780/";
         } else if ((val === "cd /instagram") || (val === "cd instagram") || (val === "cd instagram/")) {
             window.location.href = "https://www.instagram.com/degenerateddev/";
         } else if ((val === "cd /twitter") || (val === "cd twitter" || (val === "cd twitter/"))) {
             window.location.href = "https://twitter.com/degenerateddev";
         } else if ((val === "cd /linkedin") || (val === "cd linkedin") || (val === "cd linkedin/")) {
-            window.location.href = "https://www.linkedin.com/in/tim-arnold-1b1b6b1b/";
+            window.location.href = "https://www.linkedin.com/in/tim-arnold-22a035254/";
+        } else if ((val === "cd /github") || (val === "cd github") || (val === "cd github/")) {
+            window.location.href = "https://github.com/degenerateddev";
         } else {
             terminal.innerHTML += `
                 <p>Command not found...</p>
